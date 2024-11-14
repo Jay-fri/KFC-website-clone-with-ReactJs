e935b5e9552991831a0f6865384157e20ec3918c
Friday Joshua <fridayjoshua@gmail.com>
2024-11-14 05:09:11 -0800

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
